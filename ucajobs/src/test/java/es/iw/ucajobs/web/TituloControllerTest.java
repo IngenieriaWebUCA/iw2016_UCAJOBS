a525041737e6ddc6896d7572460ec41e9bc0ca65
paez7 <paez19927@hotmail.com>
2016-06-02 16:33:00 +0200

    Proyecto Primera Entrega

---
+++ ucajobs/src/test/java/es/iw/ucajobs/web/TituloControllerTest.java
@@ -0,0 +1,14 @@
+package es.iw.ucajobs.web;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.springframework.mock.staticmock.MockStaticEntityMethods;
+
+@RunWith(JUnit4.class)
+@MockStaticEntityMethods
+public class TituloControllerTest {
+
+    @Test
+    public void testMethod() {
+    }
+}
