85c0d9daa1b2e2efe1aeb0f2e3c05051db7c7d0c
paez7 <paez19927@hotmail.com>
2016-06-08 12:25:10 +0200

    Borrando

--- ucajobs/src/test/java/es/iw/ucajobs/domain/DemandanteDataOnDemand.java
+++
@@ -1,6 +0,0 @@
-package es.iw.ucajobs.domain;
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = Demandante.class)
-public class DemandanteDataOnDemand {
-}
--- ucajobs/src/test/java/es/iw/ucajobs/domain/TrabajoDataOnDemand.java
+++
@@ -1,6 +0,0 @@
-package es.iw.ucajobs.domain;
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = Trabajo.class)
-public class TrabajoDataOnDemand {
-}
